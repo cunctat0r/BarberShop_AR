666b41e4e6219a9e77e9cc0e845151cb4833e8cb
Slava Petrov <slavik--petrov@yandex.ru>
2016-01-05 13:18:30 +0300

    Add /visit page and required files

--- app.rb
+++ app.rb
@@ -12,7 +12,27 @@ end
 class Barber < ActiveRecord::Base
 end
 
+before do
+	@barbers = Barber.order "created_at DESC"
+end
+
 get '/' do
-	@barbers = Barber.all
+	
 	erb :index
+end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+  @color = params[:color]
+
+  
+  erb "Отлично, #{@username}, мастер #{@barber} будет Вас ждать в #{@datetime}"
+
 end
